515537bb05ae6a8f498469b0c3e4b9f0380df2c0
Amit Sindha <amit.sindha@gmail.com>
2024-05-01 15:31:35 +0000

    Network Setup

--- scripts/network_setup.sh
+++ scripts/network_setup.sh
@@ -10,9 +10,7 @@ SCRIPTDIR_V=$(
 source "${SCRIPTDIR_V}/environment.sh"
 source "${SCRIPTDIR_V}/utils.sh"
 
-# > ------------------
-# Public Network setup
-# File ./templates/okd_public_network.xml
+echo "Public Network setup"
 
 cp "$SCRIPTDIR_V/templates/okd_public_network.xml" "$SCRIPTDIR_V/okd_public_network.xml"
 cp "$SCRIPTDIR_V/templates/okd_private_network.xml" "$SCRIPTDIR_V/okd_private_network.xml"
@@ -21,11 +19,7 @@ f_ez_sed "<N_INT_LAN>" "$N_INT_LAN" "$SCRIPTDIR_V/okd_public_network.xml" 1
 f_ez_sed "<I_INT_LAN>" "$I_INT_LAN" "$SCRIPTDIR_V/okd_public_network.xml" 1
 f_ez_sed "<INT_LAN_24>" "$INT_LAN_24" "$SCRIPTDIR_V/okd_public_network.xml" 1
 
-# < -------------------
-
-# > ------------------
-# Private Network setup
-# File ./templates/okd_private_network.xml
+echo "Private Network setup"
 
 f_ez_sed "<N_OKD_LAN>" "$N_OKD_LAN" "$SCRIPTDIR_V/okd_private_network.xml" 1
 f_ez_sed "<OKD_LAN_24>" "$OKD_LAN_24" "$SCRIPTDIR_V/okd_private_network.xml" 1
